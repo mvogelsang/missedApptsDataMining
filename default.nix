8e71d87d7756fcbd59cb1bfa93556fc8d77fa54d
mjvoge02 <mvogelsang@rocketmail.com>
2017-12-04 17:41:55 -0500

    copy in shell for normal python workflow

---
+++ default.nix
@@ -0,0 +1,33 @@
+with import <nixpkgs> {};
+with pkgs.python27Packages;
+let
+  mplb = matplotlib.override {enableQt=true;};
+in
+  stdenv.mkDerivation {
+    name = "impurePythonEnv";
+    buildInputs = [
+      MySQL_python
+      scipy
+      pandas_0_17_1
+      cairo
+      python27Full
+      virtualenv
+      pip
+      mplb
+      taglib
+      openssl
+      git
+      libxml2
+      libxslt
+      libzip
+      stdenv
+      zlib ];
+    src = null;
+    shellHook = ''
+    # set SOURCE_DATE_EPOCH so that we can use python wheels
+    SOURCE_DATE_EPOCH=$(date +%s)
+    virtualenv --no-setuptools venv
+    export PATH=$PWD/venv/bin:$PATH
+    pip install -r requirements.txt
+    '';
+  }
